e76798bba90fad19c444499cd4692e160b96dd26
angerszhu <angers.zhu@gmail.com>
2020-01-11 14:06:44 +0800

    fix

--- src/main/scala/org/apache/spark/sql/hive/conf/KatanaConf.scala
+++ src/main/scala/org/apache/spark/sql/hive/conf/KatanaConf.scala
@@ -5,7 +5,7 @@ package org.apache.spark.sql.hive.conf
   * @date 2019/5/28 9:59
   */
 object KatanaConf {
-  val MULTI_HIVE_INSTANCE = "spark.sql.hive.schema.instances"
+  val MULTI_HIVE_INSTANCE = "spark.sql.hive.catalog.instances"
   val KATANA_INITIAL_POLL_THREAD = "spark.sql.hive.katana.initialThread"
   val KATANA_INITIAL_POLL_THREAD_ALIVE_MILLISECONDS = "spark.sql.hive.katana.initialThread.milliseconds"
   val KATANA_INITIAL_POLL_QUEUE_SIZE = "spark.sql.hive.katana.pool.queue.size"
